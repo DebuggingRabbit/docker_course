3aeb57720a131189446a67b334bf932005b04478
DebuggingRabbit <debuggingrabbit@gmail.com>
2024-05-09 23:27:05 +0100

    Update docker_course2.sh

--- docker_course2.sh
+++ docker_course2.sh
@@ -11,16 +11,16 @@ read -p "Please enter your first name: " message
 speak "Hi $message, how are you feeling today"
 
 # Prompt user for yes or no input
-speak "Do you want to join the Docker course?" & read -p "Do you want to join the Docker course? (yes/no): " response
+speak "Do you want to join the Docker course? kindly reply with a yes or a no" & read -p "Do you want to join the Docker course? (yes/no): " response
 # Speak the input
 
 
 # Check user's response
 if [ "$response" = "yes" ]; then
-    speak "Welcome to the Docker course! $message by Debugging Rabbit"
+    speak "Welcome to the Docker course! by Debugging Rabbit"
 elif [ "$response" = "no" ]; then
     speak "Disappointed to see you leave. $message"
 else
-    speak "Invalid input $message. Please enter 'yes' or 'no'."
+    speak "$message dey hear word now haba. Please enter 'yes' or 'no'."
 fi
 
